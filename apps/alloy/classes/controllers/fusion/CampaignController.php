ec95555a9606701af789016ebb5fd387b353e511
Suhmayah Banda <su.b@bandumedia.com>
2014-06-18 10:24:04 +0100

    Created campaigns controller to enable search and the return of multiple campaigns with a single request.

--- apps/alloy/classes/controllers/fusion/CampaignController.php
+++ apps/alloy/classes/controllers/fusion/CampaignController.php
@@ -4,6 +4,10 @@ namespace Controllers\Fusion;
 
 class CampaignController extends \EasePHP\Controllers\RESTful\Controller {
     
+    /**
+     * 
+     * @return \Resources\Managers\Local\Fusion\CampaignsManager
+     */
     protected function getResourceManager() {
         $db = new \Bandu\Database\MySQLWrapper(array(
                 'server' => 'localhost',
@@ -15,15 +19,24 @@ class CampaignController extends \EasePHP\Controllers\RESTful\Controller {
     }
     
     public function handleGet() {
+        if (!array_key_exists('id', $this->request)) {
+            throw new \Exception("Please provide an ID for the requested resource");
+        }
         $campaign = new \Resources\Fusion\Campaign(array(
-            'id' => 1,
+            'id' => $this->request['id'],
         ));
         $this->getResourceManager()->retrieve($campaign);
         return $campaign->render('JSON');
     }
     
     public function handlePost() {
-        
+        if (!array_key_exists('id', $this->request)) {
+            throw new \Exception("Please provide an ID for the requested resource");
+        }
+        $properties = $this->getRequestData();
+        $campaign = new \Resources\Fusion\Campaign($properties);
+        $this->getResourceManager()->create($campaign);
+        return $campaign->render('JSON');
     }
     
     public function handlePut() {
---
+++ apps/alloy/classes/controllers/fusion/CampaignsController.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Controllers\Fusion;
+
+/**
+ * Description of CampaignsController
+ *
+ * @author Bandu
+ */
+class CampaignsController extends \EasePHP\Controllers\RESTful\Controller {
+    
+    /**
+     * 
+     * @return \Resources\Managers\Local\Fusion\CampaignsManager
+     */
+    protected function getResourceManager() {
+        $db = new \Bandu\Database\MySQLWrapper(array(
+                'server' => 'localhost',
+                'user' => 'root',
+                'password' => '911DKPrince!',
+                'db' => 'test'
+        ));
+        return new \Resources\Managers\Local\Fusion\CampaignsManager($db);
+    }
+
+    public function handleDelete() {
+        throw new Exception("Method not supported for this resource");
+    }
+
+    public function handleGet() {
+        $searchCriteria = $this->getRequestData();
+        $results = $this->getResourceManager()->find($searchCriteria);
+        if (!count($results)) {
+            return \json_encode(array());
+        }
+        $formattedResults = array();
+        foreach ($results as $result) {
+            $formattedResults[] = $result->getProperties();
+        }
+        return \json_encode($formattedResults);
+    }
+
+    public function handlePost() {
+        throw new Exception("Method not supported for this resource");
+    }
+
+    public function handlePut() {
+        throw new Exception("Method not supported for this resource");
+    }
+}
